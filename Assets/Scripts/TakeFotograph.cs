8868c37e18617abbc209e44e19f8e1c56cda2962
notKimu <notkimu@proton.me>
2024-04-13 23:47:45 +0200

    Added Screenshot logic

--- Assets/Scripts/TakeFotograph.cs
+++ Assets/Scripts/TakeFotograph.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class TakeFotograph : MonoBehaviour
 {
     [SerializeField] Camera camera;
-    [SerializeField] TextMeshPro text;
     public void SaveCameraView()
     {
+        camera.enabled = true;
         int resWidth = Screen.width;
         int resHeight = Screen.height;
 
@@ -25,10 +26,36 @@ public class TakeFotograph : MonoBehaviour
         RenderTexture.active = null;
         Destroy(rt);
 
-        byte[] byteArray = screenshot.EncodeToPNG();
-        System.IO.File.WriteAllBytes("AD", byteArray);
+        Color[] pixels = screenshot.GetPixels(0, 0, resWidth, resHeight);
+
+        int minHeight = 20000;
+        int maxHeight = -1;
+        int minWidth = 20000;
+        int maxWidth = -1;
+
+        for(int i = 0; i <resHeight; i++)
+        {
+            for(int j = 0; j <resWidth; j++)
+            {
+                if (pixels[i * resWidth + j].a != 0)
+                {
+                    minHeight = Mathf.Min(minHeight, i);
+                    maxHeight = Mathf.Max(maxHeight, i);
+                    minWidth = Mathf.Min(minWidth, j);
+                    maxWidth = Mathf.Max(maxWidth, j);
+                }
+            }
+        }
+
+        Color[] col = screenshot.GetPixels(minWidth, minHeight, maxWidth - minWidth, maxHeight - minHeight);
+        Texture2D crop = new Texture2D(maxWidth - minWidth, maxHeight - minHeight, TextureFormat.RGBA32, false);
+
+        crop.SetPixels(0, 0, maxWidth - minWidth, maxHeight - minHeight, col);
+        byte[] byteArray = crop.EncodeToPNG();
+        System.IO.File.WriteAllBytes(Application.dataPath + $"\\{DateTimeOffset.UtcNow.ToUnixTimeSeconds()}.png", byteArray);
         Debug.Log("Saved");
 
+        camera.enabled = false;
 
     }
 }
